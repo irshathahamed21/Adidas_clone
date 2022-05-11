fa3426698ed22c3aafdc4a403fbd80224e06756f
irshathahamed21 <21irshathkhan@gmail.com>
2022-05-11 16:26:54 +0530

    added pages folder

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Announcement from '../../components/Announcement/Announcement'
+import TopLeftBar from '../../components/Navbar/TopLeftBar'
+
+function Home() {
+  return (
+    <div>
+        <Announcement/>
+        <TopLeftBar/>
+    </div>
+  )
+}
+
+export default Home
