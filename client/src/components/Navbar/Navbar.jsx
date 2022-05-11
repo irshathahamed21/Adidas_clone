babd65d38aaaa27351aa7a9b64fef0fb72a80f75
irshathahamed21 <21irshathkhan@gmail.com>
2022-05-11 16:26:22 +0530

    Added navabar component

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar__left">
+      <img src="" alt="" />
+      </div>
+
+      <div className="navbar__middle">
+
+      </div>
+
+      <div className="navbar__right">
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/Navbar/TopLeftBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./navbar.css"
+
+function TopLeftBar() {
+  return (
+    <div className='topLeftBar'>
+      <div className="topLeftBar__links">help</div>
+      <div className="topLeftBar__links">return</div>
+      <div className="topLeftBar__links">order tracker</div>
+      <div className="topLeftBar__links">sign up</div>
+    </div>
+  )
+}
+
+export default TopLeftBar
