d90424585e8abe9c3e6106973242ca4386c4d6ac
irshathahamed21 <21irshathkhan@gmail.com>
2022-05-18 18:38:22 +0530

    changed product card

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -4,26 +4,37 @@ import {Link} from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 
 const ProductContainer = styled.div`
-    width:200px;
+    width:303px;
     height:400px;
-
+    text-decoration:none;
+    
     &:hover {
         border:1px ridge black;
     }
+   
 
 `
-
-
 const Image = styled.img`
+      width :100%;
+      height:303px;
 
 `
+
 const Price = styled.p`
+    font-size:16px;
+    margin-top:10px;
+   
+
 
 `
 const Title = styled.p`
+    font-size:12px;
+    margin-top:10px;
 `
 
 const Text = styled.p`
+    font-size: 12px;
+    margin-top:10px;
 
 `
 
@@ -39,12 +50,11 @@ function Product() {
             products.map((e) => (
                 <ProductContainer key = {e.id}>
                          
-                      <Link to = {`/product/${e.id}`}>
-                    <Image src = {e.img[0]}/>
+                      <Link style={{textDecoration: 'none'}} to = {`/product/${e.id}`}>
+                    <Image  src = {e.img[0]} />
                     <Price>{e.price}</Price>
                     <Title>{e.name}</Title>
-                    <Text>Men Originals</Text>
-                    <Title>online exclusive</Title>  
+                    <Text>{e.collection}</Text>  
                     </Link>                
                 </ProductContainer>
             ))
